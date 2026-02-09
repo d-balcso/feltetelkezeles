8fca7e5f9af9a1ea0a7a91d70f464aa73170f622
d-balcso <deli.balint@outlook.com>
2026-02-09 21:49:39 +0100

    házi

--- feltetelek.js
+++ feltetelek.js
@@ -111,6 +111,7 @@ else if (x > 0 ){
 }
 */
 
+/*
 //Kérjünk be egy évszámot és döntsük el, hogy szökőév-e! Egy év akkor szökőév, ha az évszám maradék nélkül osztható 4-gyel, de nem osztható 100-zal, kivéve, ha az évszám osztható 400-zal.
 let szokoev = false
 let evszam = parseInt(prompt("kérek egy évet"))
@@ -127,5 +128,91 @@ if (szokEv) {
     console.log("Ez az évszám szökőév: " + evszam );
 } else {
     console.log("Ez az évszám nem szökőév: " + evszam );
-    
 }
+*/
+
+/*
+//HÁZI
+//3. Feladat
+
+let szam = parseFloat(prompt("Kérek egy számot: "));
+
+if(szam > 0){
+    console.log("A szám pozitív.");
+}else if(szam < 0){
+    console.log("A szám negatív.");
+}else {
+    console.log("A szám nulla.");
+}
+*/
+
+/*
+5. A program kérdezzen két számot, s utána írja ki a köztük lévő relációt. Például, ha a két
+szám 3 és -6.12, akkor az eredmény: 3 > -6.12.
+
+let szam4 = parseFloat(prompt("Kérem az egyik számot: "))
+let szam5 = parseFloat(prompt("Kérem a másik számot: "))
+
+if(szam4 > szam5){
+    console.log(szam4 + "A nagyobb");
+}else if(szam5 > szam4){
+    console.log(szam5 + "A nagyobb");
+}else{
+    console.log("A két szám egyenlő: " + szam4);
+}
+*/
+
+
+/*
+//14. feladat
+let honapSzam = parseInt(prompt("Kérem a hónap sorszámát (1-12): "));
+
+if(honapSzam === 1){
+    console.log("Január");
+}else if(honapSzam === 2){
+    console.log("Február");
+}else if(honapSzam === 3){
+    console.log("Március");
+}else if(honapSzam === 4){
+    console.log("Április");
+}else if(honapSzam === 5){
+    console.log("Május");
+}else if(honapSzam === 6){
+    console.log("Június");
+}else if(honapSzam === 7){
+    console.log("Július");
+}else if(honapSzam === 8){
+    console.log("Augusztus");
+}else if(honapSzam === 9){
+    console.log("Szeptember");
+}else if(honapSzam === 10){
+    console.log("Október");
+}else if(honapSzam === 11){
+    console.log("November");
+}else if(honapSzam === 12){
+    console.log("December");
+}else{
+    console.log("Rossz input, próbáld újra! ");
+}
+*/
+
+/*
+15. Kérjük be egy dolgozat pontszámát 1 és 100 között és írjuk ki az érdemjegyet! 0-40:
+elégtelen, 41-55: elégséges, 56-70: közepes, 71-85: jó, 86-100: jeles.
+
+let pontszam = parseInt(prompt("Kérem a dolgozat pontszámát (1-100): "));
+
+if(pontszam >= 0 && pontszam <= 40){
+    console.log("Elégtelen");
+}else if(pontszam >= 41 && pontszam <= 55){
+    console.log("Elégséges");
+}else if(pontszam >= 56 && pontszam <= 70){
+    console.log("Közepes");
+}else if(pontszam >= 71 && pontszam <= 85){
+    console.log("Jó");
+}else if(pontszam >= 86 && pontszam <= 100){
+    console.log("Jeles");
+}else{
+    console.log("Bibis a helyzet. ");
+}
+*/
